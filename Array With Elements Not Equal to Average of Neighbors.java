bdb8f940eaaff081b44e2adc423d32e24701b873
Saravanan V <saravananvu0404@gmail.com>
2026-07-29 08:49:16 +0530

    Array With Elements Not Equal to Average of Neighbors

---
+++ Array With Elements Not Equal to Average of Neighbors.java
@@ -0,0 +1,13 @@
+// A number can be the average of its neighbors if one neighbor is smaller than the number and the other is greater than the number.
+
+class Solution {
+    public int[] rearrangeArray(int[] nums) {
+        Arrays.sort(nums);
+        for(int i = 1; i < nums.length; i+=2){
+            int temp = nums[i];
+            nums[i] = nums[i - 1];
+            nums[i - 1] = temp;
+        }
+        return nums;
+    }
+}
